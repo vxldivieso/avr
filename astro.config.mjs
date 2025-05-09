b21ad201ac8b68dcd26f88b0a75f670cc4ae79df
vxldivieso <valdivieso.ant@gmail.com>
2025-05-09 12:38:47 -0400

    Last commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import tailwindcss from "@tailwindcss/vite";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://vxldivieso.github.io/avr/",
   base: "/avr",
-  vite: {
-    plugins: [tailwindcss()],
-  },
   integrations: [react()],
 });

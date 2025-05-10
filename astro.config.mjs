9e163591f93eb1a00a2426ad74fc5977991e300d
vxldivieso <valdivieso.ant@gmail.com>
2025-05-10 00:07:26 -0400

    FIX - Colors

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://vxldivieso.github.io/avr",
-  base: "/",
+  site: "https://vxldivieso.github.io",
+  base: "/avr",
   integrations: [react()],
 });
